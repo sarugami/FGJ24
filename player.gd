18c3ef8e7db20fe30fc298e2a00ff4e5d63ea8c8
Nikke Tapanainen <nikke@kide.games>
2024-01-26 22:28:19 +0200

    player colors

--- player.gd
+++ player.gd
@@ -2,12 +2,17 @@ extends CharacterBody3D
 
 @export var PLAYER_NUMBER = 1
 @export var SPEED = 5.0
-@onready var marker_3d = $Marker3D
+@export var PLAYER_COLOR = Color("green")
 
+@onready var marker_3d = $Marker3D
+@onready var sprite_3d = $Sprite3D
+
+func _ready():
+	sprite_3d.modulate = PLAYER_COLOR
 
 func _physics_process(delta):
-	var movement_direction = Input.get_vector("left_" + PLAYER_NUMBER, "right_" + PLAYER_NUMBER, "up_" + PLAYER_NUMBER, "down_" + PLAYER_NUMBER)
-	var aim_direction = Input.get_vector("aim_left_" + PLAYER_NUMBER, "aim_right_" + PLAYER_NUMBER, "aim_up_" + PLAYER_NUMBER, "aim_down_" + PLAYER_NUMBER)
+	var movement_direction = Input.get_vector("left_" + str(PLAYER_NUMBER), "right_" + str(PLAYER_NUMBER), "up_" + str(PLAYER_NUMBER), "down_" + str(PLAYER_NUMBER))
+	var aim_direction = Input.get_vector("aim_left_" + str(PLAYER_NUMBER), "aim_right_" + str(PLAYER_NUMBER), "aim_up_" + str(PLAYER_NUMBER), "aim_down_" + str(PLAYER_NUMBER))
 	var direction = (transform.basis * Vector3(movement_direction.x, 0, movement_direction.y)).normalized()
 	var aim = (transform.basis * Vector3(aim_direction.x, 0, aim_direction.y)).normalized()
 	if direction:
