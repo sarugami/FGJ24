e1ed16172be501da30fe51e1f46f1defeab97da0
Nikke Tapanainen <nikke@kide.games>
2024-01-27 12:01:56 +0200

    aim marker

--- player.gd
+++ player.gd
@@ -6,9 +6,9 @@ extends CharacterBody3D
 @export var DASH_SPEED = 25
 @export var PLAYER_COLOR = Color("green")
 
-@onready var marker_3d = $Marker3D
 @onready var sprite_3d = $Sprite3D
 @onready var dash_timer = $DashTimer
+@onready var marker_3d = $AttackArea/Marker3D
 
 var direction
 var can_dash = true
@@ -18,7 +18,7 @@ var dash_starting_point
 func _ready():
 	sprite_3d.modulate = PLAYER_COLOR
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	## input handling
 	var movement_direction = Input.get_vector("left_" + str(PLAYER_NUMBER), "right_" + str(PLAYER_NUMBER), "up_" + str(PLAYER_NUMBER), "down_" + str(PLAYER_NUMBER))
 	var aim_direction = Input.get_vector("aim_left_" + str(PLAYER_NUMBER), "aim_right_" + str(PLAYER_NUMBER), "aim_up_" + str(PLAYER_NUMBER), "aim_down_" + str(PLAYER_NUMBER))
@@ -26,11 +26,9 @@ func _physics_process(delta):
 	direction = (transform.basis * Vector3(movement_direction.x, 0, movement_direction.y)).normalized()
 	var aim = (transform.basis * Vector3(aim_direction.x, 0, aim_direction.y)).normalized()
 	if dash_destination:
-		velocity.x = move_toward(dash_destination.x, DASH_SPEED, 1)
-		velocity.z = move_toward(dash_destination.z, DASH_SPEED, 1)
-		print_debug("distance to dash destination: " + str(position.distance_to(dash_starting_point)))
+		velocity.x = move_toward(position.x, dash_destination.x, DASH_SPEED)
+		velocity.z = move_toward(position.z,dash_destination.z, DASH_SPEED)
 		if position.distance_to(dash_starting_point) >= DASH_LENGTH:
-			print_debug("reset dash location")
 			dash_destination = null
 	elif direction:
 		velocity.x = direction.x * SPEED
@@ -40,13 +38,15 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		
 	if aim:
-		marker_3d.position = aim_direction
+		marker_3d.rotation.y = aim_direction.y
 
 	move_and_slide()
 
 func _input(event):
 	if event.is_action_pressed("dash_" + str(PLAYER_NUMBER)):
 		dash()
+	elif event.is_action_pressed("attack_" + str(PLAYER_NUMBER)):
+		attack()
 
 func dash():
 	if direction && can_dash:
@@ -54,6 +54,9 @@ func dash():
 		dash_starting_point = position
 		dash_destination = direction * DASH_LENGTH
 		dash_timer.start()
+
+func attack():
+	pass
 
 func _on_timer_timeout():
 	can_dash = true
